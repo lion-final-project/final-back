8676f3d9b6d98cfa807796c341e06e34ccd90515
corinB <gkdisrha2020@gmail.com>
2026-01-30 19:24:18 +0900

    feat: 배달원 정보 반환을 위한 Response DTO 구현

---
+++ src/main/java/com/example/finalproject/delivery/dto/response/RiderResponse.java
@@ -0,0 +1,34 @@
+package com.example.finalproject.delivery.dto.response;
+
+import com.example.finalproject.delivery.domain.Rider;
+import com.example.finalproject.delivery.enums.RiderApprovalStatus;
+import com.example.finalproject.delivery.enums.RiderOperationStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class RiderResponse {
+
+    private Long id;
+    private Long userId;
+    private String bankName;
+    private String bankAccount;
+    private String accountHolder;
+    private RiderApprovalStatus status;
+    private RiderOperationStatus operationStatus;
+
+    public static RiderResponse from(Rider rider) {
+        return RiderResponse.builder()
+                .id(rider.getId())
+                .userId(rider.getUser().getId())
+                .bankName(rider.getBankName())
+                .bankAccount(rider.getBankAccount())
+                .accountHolder(rider.getAccountHolder())
+                .status(rider.getStatus())
+                .operationStatus(rider.getOperationStatus())
+                .build();
+    }
+}
