292bb42f54d895b8cb2a8507b420d897adc6bea1
corinB <gkdisrha2020@gmail.com>
2026-01-31 19:35:15 +0900

    feat: global 에서 S3 스토리지 서비스 및 컨트롤러 구현

--- src/main/java/com/example/finalproject/moderation/dto/request/CreateDocumentRequest.java
+++ src/main/java/com/example/finalproject/moderation/dto/request/CreateDocumentRequest.java
@@ -16,7 +16,10 @@ public class CreateDocumentRequest {
 
     @NotBlank(message = "서류 URL은 필수입니다.")
     private String documentUrl;
-
-    // document/rider/{id} /idCord, /bankbook
-    // document/store/{id} /idCord, /bankbook
+//    document/
+//     └── {userId}/           # 사용자 식별 (보안 및 관리 중심)
+//            ├── store/          # 가게 점주 관련 서류 (ApplicantType 사용)
+//            │     └── LICENSE.pdf # 파일명은 UUID_DocumentType.확장자 사용
+//            └── rider/          # 라이더 관련 서류
+//                  └── ID_CARD.png
 }
