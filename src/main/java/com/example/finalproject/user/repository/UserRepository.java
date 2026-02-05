36d468ccdabfa6fce2617fc19fc4b0c3ca05c683
KimSummerrain <doeun457@gmail.com>
2026-02-05 09:41:09 +0900

    UserRepository 복구

---
+++ src/main/java/com/example/finalproject/user/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.finalproject.user.repository;
+
+import com.example.finalproject.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByPhone(String phone);
+
+    Optional<User> findByEmail(String email);
+}
