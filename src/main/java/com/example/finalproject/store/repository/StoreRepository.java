59eb470e3c5e02f83d8cc32d137a5aaaeeb740ed
corinB <gkdisrha2020@gmail.com>
2026-02-06 14:39:41 +0900

    feat: 유저 로더 구현

--- src/main/java/com/example/finalproject/store/repository/StoreRepository.java
+++ src/main/java/com/example/finalproject/store/repository/StoreRepository.java
@@ -1,6 +1,7 @@
 package com.example.finalproject.store.repository;
 
 import com.example.finalproject.store.domain.Store;
+import com.example.finalproject.store.repository.custom.StoreRepositoryCustom;
 import com.example.finalproject.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
