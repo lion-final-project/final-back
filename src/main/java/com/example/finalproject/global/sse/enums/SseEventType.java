786c52070fc10fbc05a9e432c66742c294ab9ef0
seongsiyeong <ssy2681q@naver.com>
2026-02-12 05:30:33 +0900

    feat: 일반 결제 수정 및 기타 유틸 설정

--- src/main/java/com/example/finalproject/global/sse/enums/SseEventType.java
+++ src/main/java/com/example/finalproject/global/sse/enums/SseEventType.java
@@ -2,7 +2,9 @@ package com.example.finalproject.global.sse.enums;
 
 public enum SseEventType {
     UNREAD_COUNT("unread-count"),
-    CONNECTED("connected");
+    CONNECTED("connected"),
+    ORDER_CREATED("order-created");
+
 
     private final String eventName;
 
