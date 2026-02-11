4e4d74df0170c28545815c5a64b87abbab94939f
KimSummerrain <doeun457@gmail.com>
2026-02-11 22:19:47 +0900

    docs(rider): 기존 API 주석 복구

--- src/main/java/com/example/finalproject/delivery/controller/RiderController.java
+++ src/main/java/com/example/finalproject/delivery/controller/RiderController.java
@@ -37,12 +37,22 @@ public class RiderController {
     private final RiderService riderService;
     private final RiderLocationService riderLocationService;
 
+    /**
+     * 라이더 정보 조회
+     * @param authentication 현재 인증된 사용자 정보
+     */
     @GetMapping
     public ResponseEntity<ApiResponse<RiderResponse>> getRiderInfo(Authentication authentication) {
         RiderResponse response = riderService.getRiderInfo(authentication.getName());
         return ResponseEntity.ok(ApiResponse.success("라이더 정보 조회가 완료되었습니다.", response));
     }
 
+    /**
+     * 라이더의 영업 상태 변경
+     * @param request 라이더 영업 상태 변경 요청
+     * @param authentication 현재 인증된 사용자 정보
+     * @return 변경된 라이더 정보
+     */
     @PatchMapping("/status")
     public ResponseEntity<ApiResponse<RiderResponse>> updateOperationStatus(
             @RequestBody PatchRiderStatusRequest request,
@@ -51,6 +61,12 @@ public class RiderController {
         return ResponseEntity.ok(ApiResponse.success("라이더 상태가 변경되었습니다.", response));
     }
 
+    /**
+     * 라이더 등록 신청
+     * @param request 라이더 등록 신청 요청
+     * @param authentication 현재 인증된 사용자 정보
+     * @return 신청된 라이더 정보
+     */
     @PostMapping("/register")
     public ResponseEntity<ApiResponse<RiderApprovalResponse>> createApproval(
             @RequestBody PostRiderRegisterRequest request,
@@ -60,6 +76,11 @@ public class RiderController {
                 .body(ApiResponse.success("신청이 완료되었습니다.", response));
     }
 
+    /**
+     * 라이더 등록 신청 이력 조회
+     * @param pageable 페이지정보
+     * @param authentication 현재 인증된 사용자 정보
+     */
     @GetMapping("/approvals")
     public ResponseEntity<ApiResponse<Page<RiderApprovalResponse>>> getApprovals(
             @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
@@ -82,6 +103,10 @@ public class RiderController {
         return ResponseEntity.ok(ApiResponse.success("조회했습니다.", response));
     }
 
+    /**
+     * 라이더 등록신청 이력 삭제
+     * @param approvalsId 신청ID
+     */
     @DeleteMapping("/approvals/{approvals-id}")
     public ResponseEntity<ApiResponse<Void>> deleteApproval(@PathVariable("approvals-id") Long approvalsId) {
         riderService.deleteApproval(approvalsId);
@@ -94,12 +119,20 @@ public class RiderController {
         return ResponseEntity.ok(ApiResponse.success("라이더 위치가 업데이트되었습니다."));
     }
 
+    /**
+     * 특정 라이더 위치 조회
+     * @param riderId 레디스 라이더 식별자
+     */
     @GetMapping("/locations/{riderId}")
     public ResponseEntity<ApiResponse<GetRiderLocationResponse>> getRiderLocation(@PathVariable String riderId) {
         GetRiderLocationResponse response = riderLocationService.getRiderLocation(riderId);
         return ResponseEntity.ok(ApiResponse.success("라이더 위치 조회가 완료되었습니다.", response));
     }
 
+    /**
+     * 라이더 위치 삭제
+     * @param riderId 레디스 라이더 식별자
+     */
     @DeleteMapping("/locations/{riderId}")
     public ResponseEntity<ApiResponse<Void>> removeRiderLocation(@PathVariable String riderId) {
         riderLocationService.removeRider(riderId);
