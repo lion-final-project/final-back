96d26d3e71d5ab4a49d516ecabb72cbf7dbcb0b0
kkhw1000 <kkhw1000@naver.com>
2026-02-18 22:29:45 +0900

    refactor: 상품 수정 시 중복 방지

--- src/main/java/com/example/finalproject/product/repository/ProductRepository.java
+++ src/main/java/com/example/finalproject/product/repository/ProductRepository.java
@@ -26,6 +26,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     boolean existsByStoreAndProductNameAndDeletedAtIsNull(Store store, String productName);
 
+    boolean existsByStoreAndProductNameAndDeletedAtIsNullAndIdNot(Store store, String productName, Long id);
+
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select p from Product p where p.id = :id")
     Optional<Product> findByIdForUpdate(@Param("id") Long id);
